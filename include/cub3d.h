bf39d8f857567dc1d79e1c8b3c5c931bf91b04bf
moonsund <von.ravik@gmail.com>
2026-02-18 23:10:34 +0100

    parsing structure

--- include/cub3d.h
+++ include/cub3d.h
@@ -8,9 +8,26 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 typedef struct s_map
 {
-    char *input_str;
+    t_list *map_data;
+    int lines_count;
+
+    char *tex_N;
+    char *tex_S;
+    char *tex_E;
+    char *tex_W;
+    int floor_color;
+    int ceiling_color;
+
+
+
 
 } t_map;
 
@@ -27,6 +44,9 @@ int main(int argc, char **argv);
 // validation.c
 int validate_arguments(int argc, char **argv);
 
+
+// parse.c
+int parse_file(const char *file_path, t_map *map);
 
 // init.c
 int initialize_game(t_game *game, char *map);
--- src/parse.c
+++ src/parse.c
@@ -1,1 +1,97 @@
 #include "cub3d.h"
+
+int initialize_map(const char *file_path, t_map *map)
+{
+	if (!read_file(file_path, map))
+		return (1);
+
+	if (!procces_data(map));
+		return (1);
+
+	if (!parce_data(map));
+		return (1);
+
+	if (!validate_data(map));
+		return (1);
+
+
+}
+
+int	read_file(const char *file_path, t_map *map)
+{
+	int		fd;
+	char	*line;
+	char	*tmp;
+	t_list	*node;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd == -1)
+		return (print_error("The file is not opened.\n"));
+
+	line = get_next_line(fd);
+	if (!line)
+		return (close(fd), print_error("map file is empty.\n"));
+
+	while (line)
+	{
+		tmp = normilize(line);
+		free(line);
+		if (!tmp)
+			return (close(fd), free_lst(map->map_data), map->map_data = NULL,
+				print_error("normalize failed.\n"));
+
+		node = ft_lstnew(tmp);
+		if (!node)
+			return (free(tmp), close(fd), free_lst(map->map_data),
+				map->map_data = NULL, print_error("malloc failed.\n"));
+
+		ft_lstadd_back(&map->map_data, node);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
+
+
+int procces_data_read(map)
+{
+	while ()
+	{
+		get_colours_data()
+		
+		get_textures_data()
+	}
+
+	get_map_data()
+}	
+
+
+static int parse_map(line)
+{
+	(void)line;
+
+	return (0);
+
+}
+
+static int parse_colours(line)
+{
+	(void)line;
+	// int color = (r << 16) | (g << 8) | b;
+
+	// распарсить строку
+	// проверить:
+	// 3 числа
+	// только цифры
+	// диапазон 0–255
+	// сохранить как int color
+	return (0);
+
+}
+
+static int parse_walls(line)
+{
+	(void)line;
+	return (0);
+
+}
