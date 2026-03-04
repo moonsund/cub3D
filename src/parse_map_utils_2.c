745762c10f6891f697c9b28854e5c6d7ae64f38c
asta24326 <asta.aidar@gmail.com>
2026-03-04 23:23:00 +0100

    map parsing and validation added

---
+++ src/parse_map_utils_2.c
@@ -0,0 +1,16 @@
+#include "cub3d.h"
+
+bool	ft_tabs_check(t_map *map, int i)
+{
+	char	*line;
+
+	while (map->file_data[i])
+	{
+		line = map->file_data[i];
+		printf("linecheck passed i= [%d]\n", i);
+		if (ft_strchr(line, '\t') != NULL)
+			return (false);
+		i++;
+	}
+	return (true);
+}
